7f59c2a88ae2e0525650e124198aa23a86286340
Nikita Kislitskiy <supermrio13@gmail.com>
2018-06-12 19:35:04 +0200

    Web app + server service

--- backend/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const create_controller = require('../controllers/create_controller')
-
-module.exports = (app) => {
-    app.route('/').get(create_controller.getList);
-    app.route('/').post(create_controller.updateList);
-    app.route('/:todoId').delete(create_controller.deleteItem);
-}
